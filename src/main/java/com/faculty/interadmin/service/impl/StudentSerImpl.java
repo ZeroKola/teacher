b2e65e6b2adbe9dafa9dfb7f80ee492b9037a043
xk181fw <48406248+xk181fw@users.noreply.github.com>
2019-04-02 16:39:57 +0800

    2019.4.2

--- src/main/java/com/faculty/interadmin/service/impl/StudentSerImpl.java
+++ src/main/java/com/faculty/interadmin/service/impl/StudentSerImpl.java
@@ -1,7 +1,43 @@
 package com.faculty.interadmin.service.impl;
 
 import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.faculty.interadmin.entity.StudentEntity;
+import com.faculty.interadmin.dao.StudentDao;
+import com.faculty.interadmin.service.StudentService;
 
 @Service
-public class StudentSerImpl {
+public class StudentSerImpl implements StudentService {
+    @Autowired
+    private StudentDao studentDao;
+
+    @Override
+    public List<StudentEntity>findAll(){
+        return StudentDao.findAll();
+    }
+
+    @Override
+    @Transactional
+    public void addStudent(StudentEntity studentEntity) {
+        StudentDao.addStudent(StudentEntity);
+    }
+
+    @Override
+    @Transactional
+    public void updateStudent(StudentEntity studentEntity) {
+        StudentDao.updateStudent(StudentEntity);
+    }
+
+    @Override
+    @Transactional
+    public void deleteStudent(String id) {
+        StudentDao.deleteStudent(id);
+    }
+
+    @Override
+    public StudentEnity findById(String id) {
+        return StudentDao.findById(id);
+    }
+
 }
