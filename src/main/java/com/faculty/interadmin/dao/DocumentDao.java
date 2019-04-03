2ef68cf5f1f31eac52a2a3eb40375ef96988b370
wujiayi <wujiayi@users.noreply.github.com>
2019-04-03 15:23:39 +0800

    Revert "文件管理"
    This reverts commit f526367e4ab4726fe848311ac332c2741873e949.

--- src/main/java/com/faculty/interadmin/dao/DocumentDao.java
+++ src/main/java/com/faculty/interadmin/dao/DocumentDao.java
@@ -1,32 +1,7 @@
 package com.faculty.interadmin.dao;
 
-import com.faculty.interadmin.entity.DocumentEntity;
-import org.apache.ibatis.annotations.*;
-
-import java.util.List;
+import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
 public interface DocumentDao {
-    //    获取所有数据
-    @Select("select from document")
-    List<DocumentEntity> findAll();
-    //    根据d_no查询一条记录
-    @Select("select * from document where d_no=#{d_no}")
-    DocumentEntity getDocumentEntityByD_no(String d_no);
-    //    根据d_title查询一条记录
-    @Select("select * from document where d_title=#{d_title}")
-    DocumentEntity getDocumentEntityBydepart_title(String d_title);
-    //     增加一条记录
-    @Insert("insert into document(d_no,d_title,d_uploader,d_url,d_intro)" +
-            "values(#{d_no},#{d_title},#{d_uploader},#{d_url},#{d_intro})")
-    void insertDocumentEntity(DocumentEntity documentEntity);
-    //     更新一条记录
-    @Update("update document set d_title=#{d_title}," +
-            "d_uploader=#{d_uploader},d_url=#{d_url},d_intro=#{d_intro} where d_no=#{d_no}")
-    void updateDocumentEntity(DocumentEntity documentEntity);
-    //     删除一条记录
-    @Delete("delete from document where d_no=#{d_no}")
-    void deleteDocumentEntity(String d_no);
-
-
 }
