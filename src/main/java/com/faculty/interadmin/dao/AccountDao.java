24c5a8c35cafdf0aff2bc170d8308424b646329a
wujiayi <wujiayi@users.noreply.github.com>
2019-04-09 16:57:16 +0800

    账户表Account

--- src/main/java/com/faculty/interadmin/dao/AccountDao.java
+++ src/main/java/com/faculty/interadmin/dao/AccountDao.java
@@ -1,7 +1,21 @@
 package com.faculty.interadmin.dao;
 
-import org.apache.ibatis.annotations.Mapper;
+import com.faculty.interadmin.entity.AccountEntity;
+import org.apache.ibatis.annotations.*;
+
+import java.util.List;
 
 @Mapper
 public interface AccountDao {
+    @Select("select * from account")
+    List<AccountEntity> findAll();
+
+    @Select("select * from account where account_id=#{account_id}")
+    AccountDao getAccountByAccount_id(Integer id);
+    @Insert("insert into account(account_id,password,phone,account_type) values(#{account_id},#{password},#{phone},#{account_type})")
+    void insertAccount(AccountEntity accountEntity);
+    @Update("update account set password=#{password},phone=#{phone},account_type=#{account_type} where account_id=#{account_id}")
+    void  updateAccount(AccountEntity accountEntity);
+    @Delete("delete from account where account_id=#{account_id}")
+    void  deleteAccount(Insert account_id);
 }
--- src/main/java/com/faculty/interadmin/service/AccountService.java
+++ src/main/java/com/faculty/interadmin/service/AccountService.java
@@ -1,9 +1,17 @@
 package com.faculty.interadmin.service;
 
+import com.faculty.interadmin.entity.AccountEntity;
 import org.apache.ibatis.annotations.Mapper;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @Mapper
 public interface AccountService {
+    List<AccountEntity> findAll();
+    AccountEntity getAccountByAccount_id(Integer account_id);
+    void insertAccount(AccountEntity account);
+    void updateAccount(AccountEntity accountEntity);
+    void deleteAccount(Integer id);
 }
