a091f08442755655611fb953c242f1cc20bfbf61
ZeroKola <919487364@qq.com>
2019-04-11 19:21:24 +0800

    Update StudentSerImpl.java

--- src/main/java/com/faculty/interadmin/service/impl/StudentSerImpl.java
+++ src/main/java/com/faculty/interadmin/service/impl/StudentSerImpl.java
@@ -9,16 +9,25 @@ import org.springframework.transaction.annotation.Transactional;
 
 <<<<<<< HEAD
 =======
+import javax.swing.*;
+>>>>>>> dev-fengw
 import java.util.List;
 
 @Service
 public class StudentSerImpl implements StudentService {
+    private com.faculty.interadmin.entity.StudentEntity StudentEntity;
+
     @Autowired
 <<<<<<< HEAD
     private StudentDao studentDao;
     private Object StudentEntity;
 
     @Override
+=======
+    public StudentSerImpl(StudentDao studentDao) {
+    }
+
+>>>>>>> dev-fengw
     public List<StudentEntity> findSAll() {
         return  StudentDao.findSAll();
     }
@@ -32,6 +41,8 @@ public class StudentSerImpl implements StudentService {
 
 <<<<<<< HEAD
     @Override
+=======
+>>>>>>> dev-fengw
     @Transactional
     public void updateStudent(StudentEntity studentEntity) {
         StudentDao.updateStudent(StudentEntity);
@@ -52,6 +63,15 @@ public class StudentSerImpl implements StudentService {
     public StudentEnity findById(String id) {
 >>>>>>> dev-pjy
         return StudentDao.findSById(id);
+=======
+    public com.faculty.interadmin.entity.StudentEntity findSById(String id) {
+        return null;
+    }
+
+    @Override
+    public StudentDao findById(Spring id) {
+        return (StudentDao) StudentDao.findSById(id);
+>>>>>>> dev-fengw
     }
 
 
