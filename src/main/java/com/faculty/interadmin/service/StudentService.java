895e7f9ee80428aa25e7f82436a70102500e2e99
ZeroKola <919487364@qq.com>
2019-04-11 19:20:37 +0800

    Update StudentService.java

--- src/main/java/com/faculty/interadmin/service/StudentService.java
+++ src/main/java/com/faculty/interadmin/service/StudentService.java
@@ -1,9 +1,15 @@
 package com.faculty.interadmin.service;
 
+import com.faculty.interadmin.dao.StudentDao;
 import org.apache.ibatis.annotations.Mapper;
 import org.springframework.stereotype.Service;
 
 import com.faculty.interadmin.entity.StudentEntity;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.swing.*;
+import java.util.List;
+
 
 import java.util.List;
 
@@ -18,8 +24,29 @@ public interface StudentService {
 
     void updateStudent(StudentEntity studentEntity);
 
+=======
+
+
+    static void studentService(StudentEntity studentEntity) {
+    }
+
+
+    static List<StudentEntity> findSAll() {
+        return null;
+    }
+
+    @Transactional
+    void addStudent(StudentEntity studentEntity);
+
+     static void updateStudent() {
+        updateStudent();
+    }
+
+    @Transactional
+>>>>>>> dev-fengw
     void deleteStudent(String id);
 
     StudentEntity findSById(String id);
 
+    StudentDao findById(Spring id);
 }
