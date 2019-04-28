40a6b43950ff1bdd2df14e789ca4cf6e1885812e
wujiayi <wujiayi@users.noreply.github.com>
2019-04-28 21:25:58 +0800

    Rt_document

---
+++ src/main/java/com/faculty/interadmin/entity/Rt_document.java
@@ -0,0 +1,40 @@
+package com.faculty.interadmin.entity;
+
+public class Rt_document {
+    private String title;
+    private String uploader;
+    private String url;
+    private String intro;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getUploader() {
+        return uploader;
+    }
+
+    public void setUploader(String uploader) {
+        this.uploader = uploader;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getIntro() {
+        return intro;
+    }
+
+    public void setIntro(String intro) {
+        this.intro = intro;
+    }
+}
